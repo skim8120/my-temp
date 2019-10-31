0801fd1f9609d9c6847c91ca8911a90808298313
skim8120 <skim8120@gmail.com>
2019-10-31 11:08:16 -0400

    Finished

--- www/js/index.js
+++ www/js/index.js
@@ -44,15 +44,18 @@ var app = {
         this.receivedEvent('deviceready');
 
         var corsServer = "https://cors-anywhere.herokuapp.com/"
-        var baseURL = "https://api.nytimes.com/svc/movies/v2/reviews/picks.json?order=by-opening-date&api-key=";
+        var baseURL = "https://api.nytimes.com/svc/";
+        var parameter1 = "movies/v2/reviews/picks.json?order=by-opening-date&api-key=";
+        var parameter2 = "topstories/v2/science.json?api-key=";
         var key = "z1FTPhW4JqWZRfbsoD8Vrm6DnZD0qMQS";
-        //var parameter = "picks,integer,by-opening-date";
-        var url = baseURL + key;// + parameter;
+        var url = baseURL + parameter1 + key;
+        var url2 = baseURL + parameter2 + key;
         console.log(url);
 
         $.ajaxPrefilter(function(options) {
             if (options.crossDomain && jQuery.support.cors){
                 options.url = corsServer + options.url;
+                options.url2 = corsServer + options.url2;
                 console.log(options.url);
             }
         });
@@ -66,13 +69,17 @@ var app = {
             success: function(result){
                 console.log("Success");
                 console.log(result);
-                var response = JSON.parse(result);
+                //var response = JSON.parse(result);
+             for(var i = 0; i <= 4; i++){
 
-                $("#display_title").text(response.display_title);
-                $("#headline").text(result.headline);
-                $("#byline").text(result.byline);
-                $("#publicatoin_date").text(result.publication_date);
-                $("#linkUrl").text(result.link.url);
+                $("#weather").append("<b><h1>" + result.results[i]['display_title']  + "</h1></b><br>");
+                $("#weather").append("<p>" + "Headline: " + result.results[i]['headline'] + "</p>");
+                $("#weather").append("<p>" + "Byline: " + result.results[i]['byline'] + "</p>");
+                $("#weather").append("<p>" + "publication_date: " + result.results[i]['publication_date'] + "</p>");
+                $("#weather").append("<p>" + "link Url: " + "<a href=\"" + result.results[i].link.url + "\">"+ result.results[i].link.url + "</a>" +"</p><hr><br>");
+
+
+                }
 
             },
             error: function(xhr, status, error){
@@ -86,16 +93,55 @@ var app = {
 
         });
 
-        // $.get(baseURL + service + parameter, function(result){
-        //     //do something when success
-        //     var response = JSON.parse(result);
-        //     var div = $("#airpot");
-        //     div.append("<p>airport name=" + response.Name + "</p>");
-        //     div.append("<p>airport city=" + response.City + "</p>");
-        //     div.append("<p>airport state=" + response.State + "</p>");
-        //     div.append("<p>airport status=" + response.Status + "</p>");
-        // }, "json");
 
+         $.ajax({
+                    url: url2,
+                    type: 'GET',
+                    dataTyps:'json',
+                    headers: {'X-Requested-With': 'XMLHttpRequest'},
+                    crossDomain:true,
+                    success: function(result2){
+                        console.log("Success");
+                        console.log(result2);
+                        //var response = JSON.parse(result);
+
+                     for(var i = 0; i <= 4; i++){
+
+                        $("#story").append("<b><h1>" + result2.results[i]['title']  + "</h1></b><br>");
+                        $("#story").append("<p>" + "Byline: " + result2.results[i]['byline'] + "</p>");
+                        $("#story").append("<p>" + "publication_date: " + result2.results[i]['published_date'] + "</p>");
+                        $("#story").append("<p>" + "link Url: " + "<a href=\"" + result2.results[i]['url'] + "\">"+ result2.results[i]['url'] + "</a>" +"</p><hr><br>");
+
+
+                    }
+
+                    },
+                    error: function(xhr, status, error){
+                        console.log("Error");
+                        console.log(xhr.statusText);
+                        console.log(xhr.responseText);
+                        console.log(status);
+                        console.log(error);
+                    }
+
+
+                });
+/*
+        $.get(url2, function(result2){
+        //     //do something when success
+             console.log("Success");
+             alert("success");
+             var response = JSON.parse(result2);
+
+             for(var i = 0; i <= 4; i++){
+             var div = $("#story");
+               div.append("<p>airport name=" + response.results[i]['title'] + "</p>");
+               div.append("<p>airport city=" + response.City + "</p>");
+               div.append("<p>airport state=" + response.State + "</p>");
+               div.append("<p>airport status=" + response.Status + "</p>");
+               }
+           }, "json");
+*/
 
     },
 
@@ -112,4 +158,4 @@ var app = {
     }
 };
 
-app.initialize();g
+app.initialize();
